30890d87038b66abd226b5b1f821b192d22593de
Victoria Li <khlee1290@gmail.com>
2019-02-11 14:07:45 +0900

    Update 243.Shortest_Word_Distance.py

--- easy/243.Shortest_Word_Distance.py
+++ easy/243.Shortest_Word_Distance.py
@@ -3,19 +3,16 @@
 
 class Solution:
     def shortestDistance(self, words: 'List[str]', word1: 'str', word2: 'str') -> 'int':
-        min_distance = -1
-        pos1 = -1
-        pos2 = -1
+        min_distance = sys.maxsize
+        pos1 = sys.maxsize
+        pos2 = sys.maxsize
         for i in range(len(words)):
             if words[i] == word1:
                 pos1 = i
             if words[i] == word2:
                 pos2 = i
-            if pos1 != -1 and pos2 !=-1:
-                if min_distance == -1:
-                    min_distance = abs(pos1-pos2)
-                else:
-                    min_distance = min(min_distance, abs(pos1-pos2))
+            if pos1 != pos2:
+                min_distance = min(min_distance, abs(pos1-pos2))
         
         return min_distance
             
