cb39744ae658958c55768e976f1188ad80c07b08
Victoria Li <khlee1290@gmail.com>
2019-03-07 13:48:30 +0900

    add 168

---
+++ easy/168.Excel_Sheet_Column_Title.py
@@ -0,0 +1,14 @@
+# 168. Excel Sheet Column Title
+# https://leetcode.com/problems/excel-sheet-column-title/
+
+class Solution:
+    def convertToTitle(self, num):
+        # 实现26进制 *但26的倍数时，不进位
+        capitals = [chr(x) for x in range(ord('A'), ord('Z')+1)]
+        result = []
+        while num/26 > 0:
+            result.append(capitals[num%26-1])
+            num = (num-1)//26
+        result.reverse()
+        return ''.join(result)
+         
