1f09224e23e36c165c470a6a7efc2bd6c854e682
Victoria Li <khlee1290@gmail.com>
2019-03-07 13:55:00 +0900

    Update 168.Excel_Sheet_Column_Title.py

--- easy/168.Excel_Sheet_Column_Title.py
+++ easy/168.Excel_Sheet_Column_Title.py
@@ -6,7 +6,7 @@ class Solution:
         # 实现26进制 *但26的倍数时，不进位
         capitals = [chr(x) for x in range(ord('A'), ord('Z')+1)]
         result = []
-        while num/26 > 0:
+        while num > 0:
             result.append(capitals[num%26-1])
             num = (num-1)//26
         result.reverse()
